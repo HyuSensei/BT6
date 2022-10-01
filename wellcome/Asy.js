617fa6b98f2f64cecf57b5a478857076ebd9655c
PhanTienHuy <huyphan0220@gmail.com>
2022-10-01 10:16:20 +0700

    AsyncStorageTH

--- wellcome/Asy.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import { Text, View , StyleSheet, TextInput, TouchableOpacity, Image} from 'react-native'
-
-const Signup=()=>{
-    return (
-      <View></View>
-    )
-  }
-  export default Signup;
